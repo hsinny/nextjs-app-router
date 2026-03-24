1501ebb49c2632b26e496048ac4d68064a52f307
Hsinny <hsinnyliu.dev@gmail.com>
2026-03-24 22:47:53 +0800

    feat: add pagination and enhance search functionality in invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,11 @@
-// import Pagination from '@/app/ui/invoices/pagination';
+import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitanaFont } from '@/app/styles/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 interface PageProps {
   searchParams: Promise<{
@@ -17,6 +18,7 @@ export default async function Page(props: PageProps) {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -31,7 +33,7 @@ export default async function Page(props: PageProps) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
